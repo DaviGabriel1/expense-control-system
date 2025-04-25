8ef83a35d557e7e5d253749161f548d9bc2dff12
DaviGabriel1 <davig41441503@gmail.com>
2025-04-24 23:14:35 -0300

    Initial Commit

---
+++ src/main/java/com/payment/expensecontrolsystem/ExpenseControlSystemApplication.java
@@ -0,0 +1,13 @@
+package com.payment.expensecontrolsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseControlSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenseControlSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/payment/expensecontrolsystem/ExpenseControlSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payment.expensecontrolsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseControlSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
